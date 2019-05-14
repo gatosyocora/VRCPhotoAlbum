ba9e8a208d8c96fd217a4a03ebbcc8afe9f9f913
gatosyocora <kut190316q@gmail.com>
2019-05-14 16:55:30 +0900

    change to resident program & add func to regist/remove startup

---
+++ VRCPhotoAlbum/NofifyIconWrapper.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace VRCPhotoAlbum
+{
+    partial class NofifyIconWrapper
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region コンポーネント デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(NofifyIconWrapper));
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.toolStripMenuItem_Open = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem_OrganizePhotos = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem_StartUp = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem_Exit = new System.Windows.Forms.ToolStripMenuItem();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem_Open,
+            this.toolStripMenuItem_OrganizePhotos,
+            this.toolStripMenuItem_StartUp,
+            this.toolStripMenuItem_Exit});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(197, 92);
+            // 
+            // toolStripMenuItem_Open
+            // 
+            this.toolStripMenuItem_Open.Name = "toolStripMenuItem_Open";
+            this.toolStripMenuItem_Open.Size = new System.Drawing.Size(196, 22);
+            this.toolStripMenuItem_Open.Text = "表示";
+            // 
+            // toolStripMenuItem_OrganizePhotos
+            // 
+            this.toolStripMenuItem_OrganizePhotos.Name = "toolStripMenuItem_OrganizePhotos";
+            this.toolStripMenuItem_OrganizePhotos.Size = new System.Drawing.Size(196, 22);
+            this.toolStripMenuItem_OrganizePhotos.Text = "写真を整理";
+            // 
+            // toolStripMenuItem_StartUp
+            // 
+            this.toolStripMenuItem_StartUp.Name = "toolStripMenuItem_StartUp";
+            this.toolStripMenuItem_StartUp.Size = new System.Drawing.Size(196, 22);
+            this.toolStripMenuItem_StartUp.Text = "スタートアップに登録/解除";
+            // 
+            // toolStripMenuItem_Exit
+            // 
+            this.toolStripMenuItem_Exit.Name = "toolStripMenuItem_Exit";
+            this.toolStripMenuItem_Exit.Size = new System.Drawing.Size(196, 22);
+            this.toolStripMenuItem_Exit.Text = "終了";
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "VRCPhotoAlbum";
+            this.notifyIcon1.Visible = true;
+            this.contextMenuStrip1.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem_Open;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem_Exit;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem_OrganizePhotos;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem_StartUp;
+    }
+}
---
+++ VRCPhotoAlbum/NofifyIconWrapper.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace VRCPhotoAlbum
+{
+    public partial class NofifyIconWrapper: Component
+    {
+        private const string STARTUP_PATH = @"Software\\Microsoft\\Windows\\CurrentVersion\\Run";
+
+        public NofifyIconWrapper()
+        {
+            InitializeComponent();
+
+            toolStripMenuItem_Open.Click += this.toolStripMenuItem_Open_Click;
+            toolStripMenuItem_OrganizePhotos.Click += this.toolStripMenuItem_OrganizePhotos_Click;
+            toolStripMenuItem_StartUp.Click += this.toolStripMenuItem_StartUp_Click;
+            toolStripMenuItem_Exit.Click += this.toolStripMenuItem_Exit_Click;
+        }
+
+        public NofifyIconWrapper(IContainer container)
+        {
+            container.Add(this);
+
+            InitializeComponent();
+        }
+
+        private bool CheckRegistedAsStartUp()
+        {
+            Microsoft.Win32.RegistryKey regKey = Microsoft.Win32.Registry.CurrentUser.OpenSubKey(
+                    STARTUP_PATH, true);
+
+            return regKey.GetValue(MainWindow.APP_NAME) != null;
+        }
+
+        // https://dobon.net/vb/dotnet/system/osstartuprun.html
+        private bool RegistAsStartUpAppToCurrentUser()
+        {
+            try
+            {
+                Microsoft.Win32.RegistryKey regKey = Microsoft.Win32.Registry.CurrentUser.OpenSubKey(
+                    STARTUP_PATH, true);
+
+                if (regKey == null) return false;
+
+                Assembly assembly = Assembly.GetEntryAssembly();
+
+                if (assembly == null) return false;
+
+                regKey.SetValue(MainWindow.APP_NAME, System.Windows.Forms.Application.ExecutablePath);
+
+                regKey.Close();
+                return true;
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        private bool RemoveFromStartUpAppToCurrentUser()
+        {
+            try
+            {
+                Microsoft.Win32.RegistryKey regKey = Microsoft.Win32.Registry.CurrentUser.OpenSubKey(
+                    STARTUP_PATH, true);
+
+                if (regKey == null) return false;
+
+                Assembly assembly = Assembly.GetEntryAssembly();
+
+                if (assembly == null) return false;
+
+                regKey.DeleteValue(MainWindow.APP_NAME);
+
+                regKey.Close();
+                return true;
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        private void toolStripMenuItem_Open_Click(object sender, EventArgs e)
+        {
+            var wnd = new MainWindow();
+            wnd.Show();
+        }
+
+        private void toolStripMenuItem_OrganizePhotos_Click(object sender, EventArgs e)
+        {
+            MainWindow.MovePhotosToDayNameFolder();
+        }
+
+        private void toolStripMenuItem_StartUp_Click(object sender, EventArgs e)
+        {
+            var registedStartUp = CheckRegistedAsStartUp();
+
+            if (!registedStartUp)
+            {
+                var result = RegistAsStartUpAppToCurrentUser();
+
+                if (result)
+                {
+                    MessageBox.Show("スタートアップに登録しました");
+                }
+            }
+            else
+            {
+                var result = RemoveFromStartUpAppToCurrentUser();
+
+                if (result)
+                {
+                    MessageBox.Show("スタートアップから解除しました");
+                }
+            }
+        }
+
+        private void toolStripMenuItem_Exit_Click(object sender, EventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
