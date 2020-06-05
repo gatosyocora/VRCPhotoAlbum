92f047f9421863e7edba3c3406528b71c79ecf8b
gatosyocora <gatosyocora36@gmail.com>
2020-06-05 16:53:09 +0900

    いい感じのBindingにした

--- VRCPhotoAlbum/ViewModel/PhotoPreviewViewModel.cs
+++ VRCPhotoAlbum/ViewModel/PhotoPreviewViewModel.cs
@@ -1,26 +1,33 @@
 ﻿using Gatosyocora.VRCPhotoAlbum.Models;
+using KoyashiroKohaku.VrcMetaToolSharp;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Linq;
 using System.Text;
+using System.Windows.Media.Imaging;
 
 namespace Gatosyocora.VRCPhotoAlbum.ViewModel
 {
     public class PhotoPreviewViewModel : INotifyPropertyChanged
     {
         private Photo _previewPhoto;
-        public Photo PreviewPhoto { get => _previewPhoto; 
-                        set
-                        {
-                            _previewPhoto = value;
-                            OnPropertyChanged(nameof(PreviewPhoto));
-                        }}
+
+        public BitmapImage Image => _previewPhoto.OriginalImage;
+
+        public List<User> UserList => _previewPhoto?.MetaData?.Users ?? Enumerable.Empty<User>().ToList();
+        public string WorldName => "World: " + _previewPhoto?.MetaData?.World ?? string.Empty;
+        public string PhotographerName => "Photographer: " + _previewPhoto?.MetaData?.Photographer ?? string.Empty;
 
         public event PropertyChangedEventHandler PropertyChanged;
 
         public PhotoPreviewViewModel(Photo photo)
         {
-            PreviewPhoto = photo;
+            _previewPhoto = photo;
+            OnPropertyChanged(nameof(Image));
+            OnPropertyChanged(nameof(UserList));
+            OnPropertyChanged(nameof(WorldName));
+            OnPropertyChanged(nameof(PhotographerName));
         }
 
         protected void OnPropertyChanged(string propertyName)
