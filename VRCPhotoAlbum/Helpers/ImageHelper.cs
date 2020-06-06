5d74e4cd3cf40793c04bb0182af2657ee04a99b6
gatosyocora <gatosyocora36@gmail.com>
2020-06-07 03:09:33 +0900

    画像を回転できるようにしたが深刻な不具合があるので一旦保留

---
+++ VRCPhotoAlbum/Helpers/ImageHelper.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Drawing.Imaging;
+using KoyashiroKohaku.VrcMetaToolSharp;
+
+namespace Gatosyocora.VRCPhotoAlbum.Helpers
+{
+    public class ImageHelper
+    {
+        public static Bitmap LoadImage(string filePath)
+        {
+            if (!File.Exists(filePath))
+            {
+                throw new FileNotFoundException($"{filePath} is not found.");
+            }
+            return new Bitmap(filePath);
+        }
+
+        public static void SaveImage(Bitmap image, string filePath)
+        {
+            if (image is null)
+            {
+                throw new ArgumentNullException("image is null");
+            }
+            using (image)
+            {
+                image.Save(filePath, ImageFormat.Png);
+            }
+        }
+
+        public static void RotateLeft90AndSave(string filePath)
+        {
+            var image = LoadImage(filePath);
+            image = RotateLeft90(image);
+            SaveImage(image, filePath);
+        }
+
+        public static void RotateRight90AndSave(string filePath)
+        {
+            var image = LoadImage(filePath);
+            image = RotateRight90(image);
+            SaveImage(image, filePath);
+        }
+
+        public static Bitmap RotateLeft90(string filePath)
+        {
+            var image = LoadImage(filePath);
+            return RotateLeft90(image);
+        }
+
+        public static Bitmap RotateRight90(string filePath)
+        {
+            var image = LoadImage(filePath);
+            return RotateRight90(image);
+        }
+
+        public static Bitmap RotateLeft90(Bitmap image)
+        {
+            image.RotateFlip(RotateFlipType.Rotate270FlipNone);
+            return image;
+        }
+
+        public static Bitmap RotateRight90(Bitmap image)
+        {
+            image.RotateFlip(RotateFlipType.Rotate90FlipNone);
+            return image;
+        }
+    }
+}
--- VRCPhotoAlbum/ViewModels/PhotoPreviewViewModel.cs
+++ VRCPhotoAlbum/ViewModels/PhotoPreviewViewModel.cs
@@ -1,13 +1,16 @@
-﻿using Gatosyocora.VRCPhotoAlbum.Models;
+﻿using Gatosyocora.VRCPhotoAlbum.Helpers;
+using Gatosyocora.VRCPhotoAlbum.Models;
 using KoyashiroKohaku.VrcMetaToolSharp;
 using Reactive.Bindings;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Reactive;
 using System.Reactive.Linq;
+using System.Windows.Media.Imaging;
 
 namespace Gatosyocora.VRCPhotoAlbum.ViewModels
 {
@@ -20,7 +23,7 @@ namespace Gatosyocora.VRCPhotoAlbum.ViewModels
 
         public event PropertyChangedEventHandler PropertyChanged;
 
-        public ReadOnlyReactiveProperty<string> ImageFilePath { get; }
+        public ReadOnlyReactiveProperty<BitmapImage> Image { get; }
         public ReactiveCollection<User> UserList { get; } = new ReactiveCollection<User>();
         public ReadOnlyReactiveProperty<string> WorldName { get; }
         public ReadOnlyReactiveProperty<string> PhotographerName { get; }
@@ -31,6 +34,8 @@ namespace Gatosyocora.VRCPhotoAlbum.ViewModels
         public ReactiveCommand Next { get; set; } = new ReactiveCommand();
         public ReactiveCommand<string> SearchWithUser { get; set; } = new ReactiveCommand<string>();
         public ReactiveCommand<string> OpenTwitter { get; set; } = new ReactiveCommand<string>();
+        public ReactiveCommand RotateL90 { get; set; } = new ReactiveCommand();
+        public ReactiveCommand RotateR90 { get; set; } = new ReactiveCommand();
 
         public PhotoPreviewViewModel(Photo photo, List<Photo> photoList)
         {
@@ -38,7 +43,20 @@ namespace Gatosyocora.VRCPhotoAlbum.ViewModels
             PreviewPhoto.Value = photo;
             _previewPhotoIndex = _photoList.IndexOf(photo);
 
-            ImageFilePath = PreviewPhoto.Select(p => p?.FilePath ?? string.Empty).ToReadOnlyReactiveProperty();
+            Image = PreviewPhoto.Select(p => 
+            {
+                var filePath = p?.FilePath ?? string.Empty;
+                var bitmapImage = new BitmapImage();
+                var stream = File.OpenRead(filePath);
+                bitmapImage.BeginInit();
+                bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapImage.StreamSource = stream;
+                bitmapImage.EndInit();
+                stream.Close();
+                stream.Dispose();
+                return bitmapImage;
+            })
+            .ToReadOnlyReactiveProperty();
             WorldName = PreviewPhoto.Select(p => "World: " + p?.MetaData?.World ?? string.Empty).ToReadOnlyReactiveProperty();
             PhotographerName = PreviewPhoto.Select(p => "Photographer: " + p?.MetaData?.Photographer ?? string.Empty).ToReadOnlyReactiveProperty();
             PhotoDateTime = PreviewPhoto.Select(p => p?.MetaData?.Date?.ToString("yyyy/MM/dd HH:mm:ss") ?? string.Empty).ToReadOnlyReactiveProperty();
@@ -53,6 +71,15 @@ namespace Gatosyocora.VRCPhotoAlbum.ViewModels
             Previous.Subscribe(() => PreviousPreview());
             Next.Subscribe(() => NextPreview());
             OpenTwitter.Subscribe(userName => OpenTwitterWithScreenName(userName));
+            // 回転させるとメタデータが消えてしまうので一旦保留
+            //RotateL90.Subscribe(() =>
+            //{
+            //    ImageHelper.RotateLeft90AndSave(PreviewPhoto.Value.FilePath);
+            //});
+            //RotateR90.Subscribe(() =>
+            //{
+            //    ImageHelper.RotateRight90AndSave(PreviewPhoto.Value.FilePath);
+            //});
         }
 
         private void PreviousPreview()
@@ -87,7 +114,7 @@ namespace Gatosyocora.VRCPhotoAlbum.ViewModels
         public void Dispose()
         {
             PreviewPhoto.Dispose();
-            ImageFilePath.Dispose();
+            Image.Dispose();
             UserList.Dispose();
             WorldName.Dispose();
             PhotographerName.Dispose();
