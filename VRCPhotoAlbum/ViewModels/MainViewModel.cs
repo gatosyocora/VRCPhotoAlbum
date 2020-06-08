c9de9f9ca6291a30a6c61190b37212caa2b6c29d
gatosyocora <gatosyocora36@gmail.com>
2020-06-08 23:47:18 +0900

    日付検索も検索欄に入力しておこなうように変更

--- VRCPhotoAlbum/ViewModels/MainViewModel.cs
+++ VRCPhotoAlbum/ViewModels/MainViewModel.cs
@@ -26,12 +26,12 @@ namespace Gatosyocora.VRCPhotoAlbum.ViewModels
 
         public ReactiveProperty<string> SearchText { get; }
         public ReactiveProperty<DateTime> SearchDate { get; }
-        public ReactiveProperty<bool> SearchWithDateTime { get; }
         public ReactiveProperty<bool> HaveNoShowedPhoto { get; }
 
         public ReactiveCommand ClearSearchText { get; }
         public ReactiveCommand<Photo> ShowPreview { get; }
         public ReactiveCommand<string> SearchWithUser { get; }
+        public ReactiveCommand SearchWithDate { get; }
         public ReactiveCommand OpenSettingCommand { get; }
 
         private MainWindow _mainWindow;
@@ -50,12 +50,12 @@ namespace Gatosyocora.VRCPhotoAlbum.ViewModels
             ShowedPhotoList = new ReactiveCollection<Photo>().AddTo(Disposable);
             SearchText = new ReactiveProperty<string>(string.Empty).AddTo(Disposable);
             SearchDate = new ReactiveProperty<DateTime>(DateTime.Now).AddTo(Disposable);
-            SearchWithDateTime = new ReactiveProperty<bool>(false).AddTo(Disposable);
             HaveNoShowedPhoto = new ReactiveProperty<bool>(true).AddTo(Disposable);
 
             ClearSearchText = new ReactiveCommand().AddTo(Disposable);
             ShowPreview = new ReactiveCommand<Photo>().AddTo(Disposable);
             SearchWithUser = new ReactiveCommand<string>().AddTo(Disposable);
+            SearchWithDate = new ReactiveCommand().AddTo(Disposable);
             OpenSettingCommand = new ReactiveCommand().AddTo(Disposable);
 
             ShowedPhotoList.CollectionChanged += PhotoList_OnChanged;
@@ -75,13 +75,12 @@ namespace Gatosyocora.VRCPhotoAlbum.ViewModels
                 Debug.Print($"{e.GetType().Name}: {e.Message}");
             }
 
-            SearchText.Subscribe(searchText => SearchPhoto(searchText, SearchDate.Value, SearchWithDateTime.Value));
-            SearchDate.Subscribe(searchDate => SearchPhoto(SearchText.Value, searchDate, SearchWithDateTime.Value));
-            SearchWithDateTime.Subscribe(useDateTime => SearchPhoto(SearchText.Value, SearchDate.Value, useDateTime));
+            SearchText.Subscribe(searchText => SearchPhoto(searchText));
 
             ClearSearchText.Subscribe(() => SearchText.Value = string.Empty);
             ShowPreview.Subscribe(photo => { if (!(photo is null)) WindowHelper.OpenPhotoPreviewWindow(photo, ShowedPhotoList.ToList(), _mainWindow); });
             SearchWithUser.Subscribe(SearchWithUserName);
+            SearchWithDate.Subscribe(() => SearchWithDateString(SearchDate.Value.Date.ToString("yyyy/MM/dd HH:mm:ss")));
             OpenSettingCommand.Subscribe(() =>
             {
                 WindowHelper.OpenSettingDialog(_mainWindow);
@@ -134,18 +133,20 @@ namespace Gatosyocora.VRCPhotoAlbum.ViewModels
                         .ToList();
         }
 
-        private void SearchPhoto(string searchText, DateTime searchedDate, bool useDate)
+        private void SearchPhoto(string searchText)
         {
-            string searchUserName, searchWorldName;
+            string searchUserName, searchWorldName, searchDateString;
             var userMatch = Regex.Match(searchText, @".*user:""(?<userName>.*?)"".*");
             var worldMatch = Regex.Match(searchText, @".*world:""(?<worldName>.*?)"".*");
+            var dateMatch = Regex.Match(searchText, @".*date:""(?<dateString>.*?)"".*");
             if (userMatch.Success)
             {
                 searchUserName = $"{userMatch.Groups["userName"]}";
             }
             else
             {
-                searchUserName = Regex.Replace(searchText, @"\s*world:"".*""\s*", string.Empty);
+                searchUserName = Regex.Replace(searchText, @"\s*world:"".*?""\s*", string.Empty);
+                searchUserName = Regex.Replace(searchUserName, @"\s*date:"".*?""\s*", string.Empty);
             }
 
             if (worldMatch.Success)
@@ -154,11 +155,27 @@ namespace Gatosyocora.VRCPhotoAlbum.ViewModels
             }
             else
             {
-                searchWorldName = Regex.Replace(searchText, @"\s*user:"".*""\s*", string.Empty);
+                searchWorldName = Regex.Replace(searchText, @"\s*user:"".*?""\s*", string.Empty);
+                searchWorldName = Regex.Replace(searchWorldName, @"\s*date:"".*?""\s*", string.Empty);
             }
 
-            var searchedPhotoList = _photoList
-                    .Where(x => (!useDate || (x.MetaData?.Date?.Date.CompareTo(searchedDate.Date) ?? 1) == 0));
+            if (dateMatch.Success)
+            {
+                searchDateString = $"{dateMatch.Groups["dateString"]}";
+            }
+            else
+            {
+                searchDateString = string.Empty;
+            }
+
+            var searchedPhotoList = _photoList.Select(x => x);
+
+            if (dateMatch.Success)
+            {
+                var searchDate = DateTime.Parse(searchDateString).Date;
+                searchedPhotoList = searchedPhotoList
+                                        .Where(x => (x.MetaData?.Date?.Date.CompareTo(searchDate) ?? 1) == 0);
+            }
 
             if (!userMatch.Success && !worldMatch.Success)
             {
@@ -182,12 +199,12 @@ namespace Gatosyocora.VRCPhotoAlbum.ViewModels
 
         public void UpdatePhotoList()
         {
-            SearchPhoto(SearchText.Value, SearchDate.Value, SearchWithDateTime.Value);
+            SearchPhoto(SearchText.Value);
         }
 
         public void SearchWithUserName(string userName)
         {
-            var userMatch = Regex.Match(SearchText.Value, @"(?<prefix>.*user:"")(?<userName>.*)(?<suffix>"".*)");
+            var userMatch = Regex.Match(SearchText.Value, @"(?<prefix>.*user:"")(?<userName>.*?)(?<suffix>"".*)");
 
             if (userMatch.Success)
             {
@@ -206,7 +223,7 @@ namespace Gatosyocora.VRCPhotoAlbum.ViewModels
 
         public void SearchWithWorldName(string worldName)
         {
-            var worldMatch = Regex.Match(SearchText.Value, @"(?<prefix>.*world:"")(?<userName>.*)(?<suffix>"".*)");
+            var worldMatch = Regex.Match(SearchText.Value, @"(?<prefix>.*world:"")(?<userName>.*?)(?<suffix>"".*)");
 
             if (worldMatch.Success)
             {
@@ -225,8 +242,25 @@ namespace Gatosyocora.VRCPhotoAlbum.ViewModels
 
         public void SearchWithDateString(string dateString)
         {
-            SearchWithDateTime.Value = true;
             SearchDate.Value = DateTime.Parse(dateString).Date;
+
+            dateString = SearchDate.Value.ToString("yyyy-MM-dd");
+
+            var dateMatch = Regex.Match(SearchText.Value, @"(?<prefix>.*date:"")(?<dateString>.*?)(?<suffix>"".*)");
+
+            if (dateMatch.Success)
+            {
+                SearchText.Value = $"{dateMatch.Groups["prefix"]}{dateString}{dateMatch.Groups["suffix"]}";
+            }
+            else
+            {
+                var space = string.Empty;
+                if (!string.IsNullOrEmpty(SearchText.Value))
+                {
+                    space = " ";
+                }
+                SearchText.Value += $@"{space}date:""{dateString}""";
+            }
         }
 
         private List<string> GetSortedUserList(List<Photo> photoList)
