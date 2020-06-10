50bdc19ff4dd05b058b64073712255ade8ba614e
gatosyocora <gatosyocora36@gmail.com>
2020-06-11 07:37:28 +0900

    いい感じのテストコードになった

--- VRCPhotoAlbumTest/Models/SearchResultWorldNameTest.cs
+++ VRCPhotoAlbumTest/Models/SearchResultWorldNameTest.cs
@@ -2,11 +2,10 @@
 using KoyashiroKohaku.VrcMetaToolSharp;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Reactive.Bindings;
+using Reactive.Bindings.Extensions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace VRCPhotoAlbumTest.Models
 {
@@ -19,47 +18,52 @@ namespace VRCPhotoAlbumTest.Models
         /// → SearchTextの変化によってShowedPhotoListを更新(全削除→SearchPhoto()の検索結果を追加)
         /// </summary>
 
-        // IsSearchingのブロックがなくなれば成功する
-        [TestMethod("SearchedWorldNameで検索時にSearchTextに正しい文字列が入力されるか")]
-        public void CanSetCorrectSearchTextWhenSearchWithWorldName()
+        private IEnumerable<Photo> _photoList;
+
+        public SearchResultWorldNameTest()
+        {
+            _photoList = new int[] { 3, 2, 5, 10 } // 各ワールドが含まれる写真の数
+                            .SelectMany((i, offset) =>
+                                Enumerable.Range(0, i)
+                                    .Select(_ =>
+                                    {
+                                        var meta = new VrcMetaData
+                                        {
+                                            World = $"{(char)('a' + offset)}"
+                                        };
+                                        // ワールド検索だけどUserがnullだと自動的に検索から除外されてしまう
+                                        meta.Users.Add(new KoyashiroKohaku.VrcMetaToolSharp.User { UserName = "userName" });
+                                        return new Photo
+                                        {
+                                            MetaData = meta
+                                        };
+                                    }));
+        }
+
+        // "SearchedWorldNameで検索時にSearchTextに正しい文字列が入力されるか"
+        [DataTestMethod()]
+        [DataRow("a")]
+        [DataRow("b")]
+        [DataRow("c")]
+        [DataRow("d")]
+        public void CanSetCorrectSearchTextWhenSearchWithWorldName(string worldName)
         {
             var searchResultModel = new SearchResult(new ReactiveCollection<Photo>());
 
-            searchResultModel.SearchedWorldName.Value = "a";
-            Assert.AreEqual(@"world:""a""", searchResultModel.SearchText.Value);
-
-            searchResultModel.SearchedWorldName.Value = "b";
-            Assert.AreEqual(@"world:""b""", searchResultModel.SearchText.Value);
-
-            searchResultModel.SearchedWorldName.Value = "c";
-            Assert.AreEqual(@"world:""c""", searchResultModel.SearchText.Value);
-
-            searchResultModel.SearchedWorldName.Value = "d";
-            Assert.AreEqual(@"world:""d""", searchResultModel.SearchText.Value);
+            searchResultModel.SearchedWorldName.Value = worldName;
+            searchResultModel.SearchText.Subscribe(_ => Assert.AreEqual(@$"world:""{worldName}""", searchResultModel.SearchText.Value));
         }
 
-        [TestMethod("ワールド名の所定フォーマットから内部メソッドが正しく取得できているか")]
-        public void CanGetCorrectPhotoByBaseMethodWithFormattedWorldName()
+        // "ワールド名の所定フォーマットから内部メソッドが正しく取得できているか"
+        [DataTestMethod()]
+        [DataRow("a")]
+        [DataRow("b")]
+        [DataRow("c")]
+        [DataRow("d")]
+        public void CanGetCorrectPhotoByBaseMethodWithFormattedWorldName(string worldName)
         {
             var photoList = new ReactiveCollection<Photo>();
             var searchResultModel = new SearchResult(photoList);
-
-            var _photoList = new int[] { 3, 2, 5, 10 } // 各ワールドが含まれる写真の数
-                                .SelectMany((i, offset) =>
-                                    Enumerable.Range(0, i)
-                                        .Select(_ =>
-                                        {
-                                            var meta = new VrcMetaData
-                                            {
-                                                World = $"{(char)('a' + offset)}"
-                                            };
-                                            // ワールド検索だけどUserがnullだと自動的に検索から除外されてしまう
-                                            meta.Users.Add(new KoyashiroKohaku.VrcMetaToolSharp.User { UserName = "userName" });
-                                            return new Photo
-                                            {
-                                                MetaData = meta
-                                            };
-                                        }));
 
             foreach (var photo in _photoList)
             {
@@ -68,41 +72,20 @@ namespace VRCPhotoAlbumTest.Models
 
             var privateObject = new PrivateObject(searchResultModel);
 
-            var searchedPhotoList = privateObject.Invoke("SearchPhoto", @"world:""a""") as IEnumerable<Photo>;
-            Assert.IsTrue(searchedPhotoList.All(p => p.MetaData.World == "a"));
-
-            searchedPhotoList = privateObject.Invoke("SearchPhoto", @"world:""b""") as IEnumerable<Photo>;
-            Assert.IsTrue(searchedPhotoList.All(p => p.MetaData.World == "b"));
-
-            searchedPhotoList = privateObject.Invoke("SearchPhoto", @"world:""c""") as IEnumerable<Photo>;
-            Assert.IsTrue(searchedPhotoList.All(p => p.MetaData.World == "c"));
-
-            searchedPhotoList = privateObject.Invoke("SearchPhoto", @"world:""d""") as IEnumerable<Photo>;
-            Assert.IsTrue(searchedPhotoList.All(p => p.MetaData.World == "d"));
+            var searchedPhotoList = privateObject.Invoke("SearchPhoto", @$"world:""{worldName}""") as IEnumerable<Photo>;
+            Assert.IsTrue(searchedPhotoList.All(p => p.MetaData.World == worldName));
         }
 
-        [TestMethod("ワールド名の所定フォーマットから内部メソッドが正しい個数取得できているか")]
-        public void CanGetCorrectPhotoCountByBaseMethodWithFormattedWorldName()
+        // "ワールド名の所定フォーマットから内部メソッドが正しい個数取得できているか"
+        [DataTestMethod()]
+        [DataRow("a", 3)]
+        [DataRow("b", 2)]
+        [DataRow("c", 5)]
+        [DataRow("d", 10)]
+        public void CanGetCorrectPhotoCountByBaseMethodWithFormattedWorldName(string worldName, int hitCount)
         {
             var photoList = new ReactiveCollection<Photo>();
             var searchResultModel = new SearchResult(photoList);
-
-            var _photoList = new int[] { 3, 2, 5, 10 } // 各ワールドが含まれる写真の数
-                                .SelectMany((i, offset) =>
-                                    Enumerable.Range(0, i)
-                                        .Select(_ =>
-                                        {
-                                            var meta = new VrcMetaData
-                                            {
-                                                World = $"{(char)('a' + offset)}"
-                                            };
-                                            // ワールド検索だけどUserがnullだと自動的に検索から除外されてしまう
-                                            meta.Users.Add(new KoyashiroKohaku.VrcMetaToolSharp.User { UserName = "userName" });
-                                            return new Photo
-                                            {
-                                                MetaData = meta
-                                            };
-                                        }));
 
             foreach (var photo in _photoList)
             {
@@ -111,100 +94,52 @@ namespace VRCPhotoAlbumTest.Models
 
             var privateObject = new PrivateObject(searchResultModel);
 
-            var searchedPhotoList = (IEnumerable<Photo>)privateObject.Invoke("SearchPhoto", @"world:""a""");
-            Assert.AreEqual(3, searchedPhotoList.Count());
-
-            searchedPhotoList = privateObject.Invoke("SearchPhoto", @"world:""b""") as IEnumerable<Photo>;
-            Assert.AreEqual(2, searchedPhotoList.Count());
-
-            searchedPhotoList = privateObject.Invoke("SearchPhoto", @"world:""c""") as IEnumerable<Photo>;
-            Assert.AreEqual(5, searchedPhotoList.Count());
-
-            searchedPhotoList = privateObject.Invoke("SearchPhoto", @"world:""d""") as IEnumerable<Photo>;
-            Assert.AreEqual(10, searchedPhotoList.Count());
+            var searchedPhotoList = (IEnumerable<Photo>)privateObject.Invoke("SearchPhoto", @$"world:""{worldName}""");
+            Assert.AreEqual(hitCount, searchedPhotoList.Count());
         }
 
-        [TestMethod("SearchTestに検索するワールド名の所定フォーマットを入力して正しく取得できているか")]
-        public void CanGetCorrectPhotoWhenInputFormattedWorldNameToSearchText()
+        // "SearchTestに検索するワールド名の所定フォーマットを入力して正しく取得できているか"
+        [DataTestMethod()]
+        [DataRow("a", 3)]
+        [DataRow("b", 2)]
+        [DataRow("c", 5)]
+        [DataRow("d", 10)]
+        public void CanGetCorrectPhotoWhenInputFormattedWorldNameToSearchText(string worldName, int hitCount)
         {
             var photoList = new ReactiveCollection<Photo>();
             var searchResultModel = new SearchResult(photoList);
-
-            var _photoList = new int[] { 3, 2, 5, 10 } // 各ワールドが含まれる写真の数
-                                .SelectMany((i, offset) =>
-                                    Enumerable.Range(0, i)
-                                        .Select(_ =>
-                                        {
-                                            var meta = new VrcMetaData
-                                            {
-                                                World = $"{(char)('a' + offset)}"
-                                            };
-                                            // ワールド検索だけどUserがnullだと自動的に検索から除外されてしまう
-                                            meta.Users.Add(new KoyashiroKohaku.VrcMetaToolSharp.User { UserName = "userName" });
-                                            return new Photo
-                                            {
-                                                MetaData = meta
-                                            };
-                                        }));
-
 
             foreach (var photo in _photoList)
             {
                 photoList.Add(photo);
             }
 
-            searchResultModel.SearchText.Value = @"world:""a""";
-            Assert.AreEqual(3, searchResultModel.ShowedPhotoList.Count);
-
-            searchResultModel.SearchText.Value = @"world:""b""";
-            Assert.AreEqual(2, searchResultModel.ShowedPhotoList.Count);
-
-            searchResultModel.SearchText.Value = @"world:""c""";
-            Assert.AreEqual(5, searchResultModel.ShowedPhotoList.Count);
-
-            searchResultModel.SearchText.Value = @"world:""d""";
-            Assert.AreEqual(10, searchResultModel.ShowedPhotoList.Count);
+            searchResultModel.SearchText.Value = @$"world:""{worldName}""";
+            searchResultModel.ShowedPhotoList.ObserveAddChangedItems()
+                .Subscribe(_ => Assert.AreEqual(hitCount, searchResultModel.ShowedPhotoList.Count));
         }
 
+        // "SearchTestに検索するワールド名の所定フォーマットを入力して正しく取得できているか"
+        [DataTestMethod()]
+        [DataRow("a", 3)]
+        [DataRow("b", 2)]
+        [DataRow("c", 5)]
+        [DataRow("d", 10)]
         [TestMethod("SearchedWorldNameに検索するワールド名を入力して正しく取得できているか")]
-        public void CanGetCorrectPhotoWhenSearchWithWorldName()
+        public void CanGetCorrectPhotoWhenSearchWithWorldName(string worldName, int hitCount)
         {
             var photoList = new ReactiveCollection<Photo>();
             var searchResultModel = new SearchResult(photoList);
-
-            var _photoList = new int[] { 3, 2, 5, 10 } // 各ワールドが含まれる写真の数
-                                .SelectMany((i, offset) =>
-                                    Enumerable.Range(0, i)
-                                        .Select(_ =>
-                                        {
-                                            var meta = new VrcMetaData
-                                            {
-                                                World = $"{(char)('a' + offset)}"
-                                            };
-                                            // ワールド検索だけどUserがnullだと自動的に検索から除外されてしまう
-                                            meta.Users.Add(new KoyashiroKohaku.VrcMetaToolSharp.User { UserName = "userName" });
-                                            return new Photo
-                                            {
-                                                MetaData = meta
-                                            };
-                                        }));
 
             foreach (var photo in _photoList)
             {
                 photoList.Add(photo);
             }
 
-            searchResultModel.SearchedWorldName.Value = "a";
-            Assert.AreEqual(3, searchResultModel.ShowedPhotoList.Count);
+            searchResultModel.SearchedWorldName.Value = worldName;
 
-            searchResultModel.SearchedWorldName.Value = "b";
-            Assert.AreEqual(2, searchResultModel.ShowedPhotoList.Count);
-
-            searchResultModel.SearchedWorldName.Value = "c";
-            Assert.AreEqual(5, searchResultModel.ShowedPhotoList.Count);
-
-            searchResultModel.SearchedWorldName.Value = "d";
-            Assert.AreEqual(10, searchResultModel.ShowedPhotoList.Count);
+            searchResultModel.ShowedPhotoList.ObserveAddChangedItems()
+                .Subscribe(_ => Assert.AreEqual(hitCount, searchResultModel.ShowedPhotoList.Count));
         }
     }
 }
