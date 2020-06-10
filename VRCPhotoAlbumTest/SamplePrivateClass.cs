605e3d4c47bd5ba305d009d07a9c5dad69097e83
gatosyocora <gatosyocora36@gmail.com>
2020-06-11 06:50:39 +0900

    PrivateObjectクラスを試すためのプライベートメソッドを持ったクラスを作成

---
+++ VRCPhotoAlbumTest/SamplePrivateClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VRCPhotoAlbumTest
+{
+    public class SamplePrivateClass
+    {
+        public SamplePrivateClass()
+        {
+
+        }
+
+        private string PrivateMethod()
+        {
+            return "hoge";
+        }
+    }
+}
