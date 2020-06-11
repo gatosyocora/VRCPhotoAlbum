234e110d5437c47562c3c75b66d505b122bee005
gatosyocora <gatosyocora36@gmail.com>
2020-06-11 20:27:19 +0900

    MainViewModelのテストクラスを作成

---
+++ VRCPhotoAlbumTest/ViewModels/MainViewModelTest.cs
@@ -0,0 +1,18 @@
+﻿using Gatosyocora.VRCPhotoAlbum.ViewModels;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+
+namespace VRCPhotoAlbumTest.ViewModels
+{
+    [TestClass]
+    public class MainViewModelTest
+    {
+        [TestMethod("MainViewModelをインスタンス化させたときにnullではない")]
+        public void IsNotNull()
+        {
+        }
+    }
+}
