770400895c6a1757f1ee9bdbf7ce7b3d216cc101
gatosyocora <gatosyocora36@gmail.com>
2020-06-11 13:21:57 +0900

    SearchResultのユーザーに関するテストを作成

--- VRCPhotoAlbumTest/Models/SearchResultUserNameTest.cs
+++ VRCPhotoAlbumTest/Models/SearchResultUserNameTest.cs
@@ -2,6 +2,7 @@
 using KoyashiroKohaku.VrcMetaToolSharp;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Reactive.Bindings;
+using Reactive.Bindings.Extensions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,80 +19,130 @@ namespace VRCPhotoAlbumTest.Models
         /// → SearchTextの変化によってShowedPhotoListを更新(全削除→SearchPhoto()の検索結果を追加)
         /// </summary>
 
-        [TestMethod("SearchedUserNameで検索時にSearchTextに正しい文字列が入力されるか")]
-        public void CanSetCorrectSearchTextWhenSearchWithUserName()
+        private List<Photo> _photoList;
+
+        public SearchResultUserNameTest()
         {
-            var photoList = new ReactiveCollection<Photo>();
-            var searchResultModel = new SearchResult(photoList);
-
             var _photoList = new int[] { 3, 2, 5, 10 } // 各人が含まれる写真の数
-                                .SelectMany((i, offset) =>
-                                {
-                                    var meta = new VrcMetaData();
-                                    meta.Users.Add(
-                                        new KoyashiroKohaku.VrcMetaToolSharp.User
-                                        {
-                                            UserName = ('a' + offset).ToString()
-                                        });
-                                    return Enumerable.Range(0, i)
-                                    .Select(_ =>
-                                        new Photo
-                                        {
-                                            MetaData = meta
-                                        });
-                                });
-
-            photoList.AddRangeOnScheduler(_photoList);
-
-            searchResultModel.SearchedUserName.Value = "a";
-            Assert.AreEqual(@"user:""a""", searchResultModel.SearchText.Value);
-
-            searchResultModel.SearchedUserName.Value = "b";
-            Assert.AreEqual(@"user:""b""", searchResultModel.SearchText.Value);
-
-            searchResultModel.SearchedUserName.Value = "c";
-            Assert.AreEqual(@"user:""c""", searchResultModel.SearchText.Value);
-
-            searchResultModel.SearchedUserName.Value = "d";
-            Assert.AreEqual(@"user:""d""", searchResultModel.SearchText.Value);
+                    .SelectMany((i, offset) =>
+                    {
+                        var meta = new VrcMetaData 
+                        {
+                            World = "worldName"
+                        };
+                        meta.Users.Add(
+                            new KoyashiroKohaku.VrcMetaToolSharp.User
+                            {
+                                UserName = $"{(char)('a' + offset)}"
+                            });
+                        return Enumerable.Range(0, i)
+                        .Select(_ =>
+                            new Photo
+                            {
+                                MetaData = meta,
+                            });
+                    });
         }
 
-        [TestMethod("SearchedUserNameに検索するユーザー名を入力して正しく取得できているか")]
-        public void CanGetCorrectPhotoWhenSearchWithUserName()
+        [TestMethod("SearchedUserNameで検索時にSearchTextに正しい文字列が入力されるか")]
+        [DataTestMethod]
+        [DataRow("a")]
+        [DataRow("b")]
+        [DataRow("c")]
+        [DataRow("d")]
+        public void CanSetCorrectSearchTextWhenSearchWithUserName(string userName)
+        {
+            var searchResultModel = new SearchResult(new ReactiveCollection<Photo>());
+
+            searchResultModel.SearchedUserName.Value = userName;
+            searchResultModel.SearchText.Subscribe(_ => Assert.AreEqual(@$"user:""{userName}""", searchResultModel.SearchText.Value));
+        }
+
+        [TestMethod("ユーザー名の所定フォーマットから内部メソッドが正しく取得できているか")]
+        [DataTestMethod]
+        [DataRow("a")]
+        [DataRow("b")]
+        [DataRow("c")]
+        [DataRow("d")]
+        public void CanGetCorrectPhotoByBaseMethodWithFormattedUserName(string userName)
         {
             var photoList = new ReactiveCollection<Photo>();
             var searchResultModel = new SearchResult(photoList);
 
-            var _photoList = new int[] { 3, 2, 5, 10 } // 各人が含まれる写真の数
-                                .SelectMany((i, offset) =>
-                                {
-                                    var meta = new VrcMetaData();
-                                    meta.Users.Add(
-                                        new KoyashiroKohaku.VrcMetaToolSharp.User
-                                        {
-                                            UserName = ('a' + offset).ToString()
-                                        });
-                                    return Enumerable.Range(0, i)
-                                    .Select(_ =>
-                                        new Photo
-                                        {
-                                            MetaData = meta
-                                        });
-                                });
+            foreach (var photo in _photoList)
+            {
+                photoList.Add(photo);
+            }
 
-            photoList.AddRangeOnScheduler(_photoList);
+            var privateObject = new PrivateObject(searchResultModel);
 
-            searchResultModel.SearchedUserName.Value = "a";
-            Assert.AreEqual(3, searchResultModel.ShowedPhotoList.Count);
+            var searchedPhotoList = privateObject.Invoke("SearchPhoto", @$"user:""{userName}""") as IEnumerable<Photo>;
+            Assert.IsTrue(searchedPhotoList.All(p => p.MetaData.Users.Any(u => u.UserName == userName)));
+        }
 
-            searchResultModel.SearchedUserName.Value = "b";
-            Assert.AreEqual(2, searchResultModel.ShowedPhotoList.Count);
+        [TestMethod("ユーザー名の所定フォーマットから内部メソッドが正しい個数取得できているか")]
+        [DataTestMethod]
+        [DataRow("a", 3)]
+        [DataRow("b", 2)]
+        [DataRow("c", 5)]
+        [DataRow("d", 10)]
+        public void CanGetCorrectPhotoCountByBaseMethodWithFormattedUserName(string userName, int hitCount)
+        {
+            var photoList = new ReactiveCollection<Photo>();
+            var searchResultModel = new SearchResult(photoList);
 
-            searchResultModel.SearchedUserName.Value = "c";
-            Assert.AreEqual(5, searchResultModel.ShowedPhotoList.Count);
+            foreach (var photo in _photoList)
+            {
+                photoList.Add(photo);
+            }
 
-            searchResultModel.SearchedUserName.Value = "d";
-            Assert.AreEqual(10, searchResultModel.ShowedPhotoList.Count);
+            var privateObject = new PrivateObject(searchResultModel);
+
+            var searchedPhotoList = (IEnumerable<Photo>)privateObject.Invoke("SearchPhoto", @$"user:""{userName}""");
+            Assert.AreEqual(hitCount, searchedPhotoList.Count());
+        }
+
+        [TestMethod("SearchTextに検索するユーザー名の所定フォーマットを入力して正しく取得できているか")]
+        [DataTestMethod]
+        [DataRow("a", 3)]
+        [DataRow("b", 2)]
+        [DataRow("c", 5)]
+        [DataRow("d", 10)]
+        public void CanGetCorrectPhotoWhenInputFormattedUserNameToSearchText(string userName, int hitCount)
+        {
+            var photoList = new ReactiveCollection<Photo>();
+            var searchResultModel = new SearchResult(photoList);
+
+            foreach (var photo in _photoList)
+            {
+                photoList.Add(photo);
+            }
+
+            searchResultModel.SearchText.Value = @$"user:""{userName}""";
+            searchResultModel.ShowedPhotoList.ObserveAddChangedItems()
+                .Subscribe(_ => Assert.AreEqual(hitCount, searchResultModel.ShowedPhotoList.Count));
+        }
+
+        [TestMethod("SearchTextに検索するユーザー名の所定フォーマットを入力して正しく取得できているか")]
+        [DataTestMethod]
+        [DataRow("a", 3)]
+        [DataRow("b", 2)]
+        [DataRow("c", 5)]
+        [DataRow("d", 10)]
+        public void CanGetCorrectPhotoWhenSearchWithUserName(string userName, int hitCount)
+        {
+            var photoList = new ReactiveCollection<Photo>();
+            var searchResultModel = new SearchResult(photoList);
+
+            foreach (var photo in _photoList)
+            {
+                photoList.Add(photo);
+            }
+
+            searchResultModel.SearchedUserName.Value = userName;
+
+            searchResultModel.ShowedPhotoList.ObserveAddChangedItems()
+                .Subscribe(_ => Assert.AreEqual(hitCount, searchResultModel.ShowedPhotoList.Count));
         }
     }
 }
