539fcb240fb36e0a58e8bd0957b936dc7b6f9c70
gatosyocora <gatosyocora36@gmail.com>
2020-06-17 15:08:04 +0900

    参照するdbのパスが間違っていたので修正

--- VRCPhotoAlbum/Servisies/DBCacheService.cs
+++ VRCPhotoAlbum/Servisies/DBCacheService.cs
@@ -66,7 +66,7 @@ namespace Gatosyocora.VRCPhotoAlbum.Servisies
 
             if (_context is null)
             {
-                _context = new Context();
+                _context = new Context(_dbFilePath);
             }
         }
 
