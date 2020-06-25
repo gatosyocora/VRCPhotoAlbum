359655ec474df3c7a3f8bc8773fd0fb6b73e1752
gatosyocora <gatosyocora36@gmail.com>
2020-06-26 03:55:01 +0900

    デザイナー用に設定画面のダミーデータを作成

---
+++ VRCPhotoAlbum/ViewModels/Designer/DummySettingViewModel.cs
@@ -0,0 +1,30 @@
+﻿using Gatosyocora.VRCPhotoAlbum.Models;
+using Reactive.Bindings;
+using Reactive.Bindings.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gatosyocora.VRCPhotoAlbum.ViewModels.Designer
+{
+    [Obsolete("Designer only", true)]
+    public class DummySettingViewModel : ViewModelBase
+    {
+        public ReactiveCollection<PhotoFolder> PhotoFolders { get; }
+        public ReactiveProperty<string> CacheDataSize { get; }
+        public ReactiveProperty<string> CacheFolderPath { get; }
+        public ReactiveProperty<bool> CanEnter { get; }
+        public ReactiveProperty<string> MessageText { get; }
+
+        public DummySettingViewModel()
+        {
+            PhotoFolders = new ReactiveCollection<PhotoFolder>().AddTo(Disposable);
+            PhotoFolders.Add(new PhotoFolder { FolderPath=@"C:\\Test", ContainsSubFolder=false});
+            PhotoFolders.Add(new PhotoFolder { FolderPath=@"C:\\Hoge\\VRChat", ContainsSubFolder=true});
+            CacheDataSize = new ReactiveProperty<string>("00 MB").AddTo(Disposable);
+            CacheFolderPath = new ReactiveProperty<string>(@"C:\\Hoge\\Piyo").AddTo(Disposable);
+            CanEnter = new ReactiveProperty<bool>(false).AddTo(Disposable);
+            MessageText = new ReactiveProperty<string>("Testメッセージです").AddTo(Disposable);
+        }
+    }
+}
